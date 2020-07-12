fb055b2b4234e9a5a3c592191b19a76a65ec692e
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-12 14:23:10 -0300

    Ejercicio 1 del practico 3

---
+++ SeleniumJava/src/Practico3_Ejercicio1.java
@@ -0,0 +1,13 @@
+
+public class Practico3_Ejercicio1 {
+
+	    public static void main(String args[]) {
+	        int duplicate = calculateDouble(7);
+	        System.out.println("El doble del numero es " + duplicate);
+
+	    }
+
+	    public static int calculateDouble (int num) {
+	        return num * 2; 
+	    } 
+	}
