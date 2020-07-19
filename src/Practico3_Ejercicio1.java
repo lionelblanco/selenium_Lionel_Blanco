2d773c49e39dbeeb4de120486eacde12cbb8397b
Lionel-Blanco <mlionelblanco@gmail.com>
2020-07-19 18:02:02 +0200

    Metodos - Practico 3 - Ejercicio1

---
+++ src/Practico3_Ejercicio1.java
@@ -0,0 +1,18 @@
+//1)Escribir un método que reciba un número y retorne su doble
+
+public class Practico3_Ejercicio1 {
+
+    public static void main(String args[]) {
+        int duplicate = calculateDouble(7);
+        System.out.println("El doble del numero es " + duplicate);
+
+    }
+
+    public static int calculateDouble (int num) {
+        return num * 2;
+    }
+}
+
+
+
+
