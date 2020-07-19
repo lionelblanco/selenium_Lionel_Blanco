35d838ee18889227a6269e17413eda9ef236f5f4
Lionel-Blanco <mlionelblanco@gmail.com>
2020-07-19 18:11:33 +0200

    Metodos - Practico 3 - Ejercicio 3

---
+++ src/Practico3_Ejercicio3.java
@@ -0,0 +1,25 @@
+//3)Método que reciba dos enteros y retorne la multiplicación de la misma. 
+
+import java.util.Scanner;
+
+public class Practico3_Ejercicio3 {
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese un número: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Ingrese el otro número: ");
+        int num2 = input.nextInt();
+
+        System.out.println("El resultado de la multiplicación es " + multiplicacion(num1,num2));
+    }
+
+    public static int multiplicacion(int num1, int num2){
+        return num1 * num2;
+    }
+}
+
+
+
