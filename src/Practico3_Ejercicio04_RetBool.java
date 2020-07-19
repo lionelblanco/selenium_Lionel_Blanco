b93ed657da43bcabb6b607637b8de9e18d7c897e
Lionel-Blanco <mlionelblanco@gmail.com>
2020-07-19 21:22:43 +0200

    Metodos con retorno Booleano - Practico 3 - Ejercicio 4

---
+++ src/Practico3_Ejercicio04_RetBool.java
@@ -0,0 +1,27 @@
+//04)Realizar un método que retorne verdadero si el número recibido por parámetro es impar
+
+import java.util.Scanner;
+
+public class Practico3_Ejercicio04_RetBool {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+        int num = input.nextInt();
+        boolean impar = esImpar(num);
+
+        if (impar == true) {
+            System.out.println("El numero es impar");
+        } else {
+            System.out.println("El numero es par ");
+        }
+    }
+
+    public static boolean esImpar(int num) {
+        if (num % 2 == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
