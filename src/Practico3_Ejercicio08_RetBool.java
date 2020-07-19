3c6c3e4c423e0b4d99458df34426023244fce177
Lionel-Blanco <mlionelblanco@gmail.com>
2020-07-19 22:09:46 +0200

    Metodos con retorno Booleano - Practico 3 - Ejercicio 8

---
+++ src/Practico3_Ejercicio08_RetBool.java
@@ -0,0 +1,31 @@
+/*Realizar un método llamado esMayor que reciba un número, y retorne verdadero si el número es mayor a 0,
+de lo contrario, se debe retornar falso. Luego, solicitar al usuario que ingrese un número.
+Sii es positivo, se debe imprimir: “El número es positivo”, de lo contrario,
+“El número es negativo” (Se debe utilizar el método realizado anteriormente)*/
+
+import java.util.Scanner;
+
+public class Practico3_Ejercicio08_RetBool {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresar Un numero");
+        int num = input.nextInt();
+
+        boolean mayor = esMayor(num);
+
+        if (mayor == true){
+            System.out.println("El numero es positivo");
+        }else {
+            System.out.println("El numero es negativo");
+
+        }
+
+    }
+    public static boolean esMayor (int num){
+        if (num > 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
