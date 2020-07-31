89054f6cf70cc5029be78a9d2e5c46caae514566
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-30 23:51:12 -0300

    Metodos - Practico 3 - Ejercicio 05

---
+++ PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio05.java
@@ -0,0 +1,28 @@
+//5)Método que reciba un número entero y un número double, y retorne su multiplicación.
+//Que tipo de retorno tiene este método?
+
+
+
+package Practico3_Metodos;
+
+import java.util.Scanner;
+
+public class Practico3_Ejercicio05 {
+
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese un número: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Ingrese el otro número: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("La multiplicación es " + multiplicacion(num1,num2));
+    }
+
+    public static double multiplicacion(int num1, double num2){
+        double multiplicacion = num1 * num2;
+        return multiplicacion;
+    }
+}
