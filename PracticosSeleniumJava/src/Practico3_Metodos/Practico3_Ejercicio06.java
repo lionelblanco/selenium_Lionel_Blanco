f55ddab29c4e9624c34840d644ce9c1b0b2cd83b
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-31 00:00:07 -0300

    Metodos Pracitco 3 - Ejercicio 06

---
+++ PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio06.java
@@ -0,0 +1,29 @@
+package Practico3_Metodos;
+
+import java.util.Scanner;
+
+public class Practico3_Ejercicio06 {
+	public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+
+	System.out.println("Ingrese un número: ");
+	int num1 = input.nextInt();
+
+	System.out.println("Ingrese el otro número: ");
+	int num2 = input.nextInt();
+    
+	if (mayorDeDosNumeros(num1, num2) == true) {
+	   System.out.println("El mayor de los dos números es: " + num1);
+	  } else {
+	   System.out.println("El mayor de los dos números es: " + num2);
+	        }
+	    }
+
+	    public static boolean mayorDeDosNumeros(int num1, int num2) {
+	        if (num1 > num2) {
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+	}
