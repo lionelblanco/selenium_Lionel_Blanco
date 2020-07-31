419aaaa55583507e3b6e8a211430d2ee052bb4ab
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-31 00:18:31 -0300

    Save changes

--- PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio10.java
+++ PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio10.java
@@ -1,10 +1,23 @@
 package Practico3_Metodos;
 
+import java.util.Scanner;
+
 public class Practico3_Ejercicio10 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
 
-	}
+        System.out.println("Ingrese el alto");
+        float alto = input.nextFloat();
+        System.out.println("Ingrese el ancho");
+        float ancho = input.nextFloat();
 
+        System.out.println("El area del paralelogramo es "+ areaPararelogramoTipo(alto,ancho));
+
+    }
+
+    public static float areaPararelogramoTipo(float alto, float ancho){
+        float area = alto * ancho;
+        return area;
+    }
 }
