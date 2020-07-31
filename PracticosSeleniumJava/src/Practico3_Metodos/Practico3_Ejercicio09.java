55cd9cbfca91d5dd1d12306be640df2a49754053
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-31 00:13:41 -0300

    Metodos - Practico 3 - Ejercicio 09

---
+++ PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio09.java
@@ -0,0 +1,27 @@
+/*9)Crear un método que permita reciba el radio de un círculo, y
+ retorne su area (radio^2)*PI (utilizar una variable estática para el valor de PI)
+ */
+
+
+package Practico3_Metodos;
+
+import java.util.Scanner;
+
+public class Practico3_Ejercicio09 {
+
+	 public static final double PI = 3.14;
+	    public static void main(String args[]) {
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.println("Ingrese el radio del círculo");
+	        double radio = input.nextDouble();
+
+	        System.out.println("El área del círculo es " + areaCirculo(radio));
+
+	    }
+
+	    public static double areaCirculo (double radio){
+	        double area =  radio * radio * PI;
+	        return area;
+	    }
+	}
