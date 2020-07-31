995cfb05fcc1e881f2ade6adee946fd8feb4d87d
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-31 00:20:51 -0300

    save c hanges

--- PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio11.java
+++ PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio11.java
@@ -1,10 +1,26 @@
+//11)Crear un método que reciba un entero, y muestre en pantalla la cantidad de dólares y
+// de euros que representa ese número
+
 package Practico3_Metodos;
+
+import java.util.Scanner;
 
 public class Practico3_Ejercicio11 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static float EURO = 50;
+    public static float DOLAR = 42;
 
-	}
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Ingrese un numero");
+        float numero = input.nextFloat();
+        conversionMonedas(22);
+    }
+
+    public static void conversionMonedas(int numero){
+        System.out.println(numero / EURO);
+        System.out.println(numero / DOLAR);
+
+    }
 }
