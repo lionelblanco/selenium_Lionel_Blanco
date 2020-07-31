a3401374225a4eb957d6e23887cb5a050d4d8264
Lionel Blanco <mlionelblanco@gmail.com>
2020-07-31 00:36:14 -0300

    Cambio

--- PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio12.java
+++ PracticosSeleniumJava/src/Practico3_Metodos/Practico3_Ejercicio12.java
@@ -1,10 +1,25 @@
+/*12)Realizar un método llamado calcularIva, que calcule el IVA (22%) dado un
+número float llamado sueldo, recibido por parámetro. Sugerencia: utilizar variables estáticas.
+ */
+
 package Practico3_Metodos;
+
+import java.util.Scanner;
 
 public class Practico3_Ejercicio12 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static final int IVA = 22;
+	    public static void main(String args[]) {
+	        Scanner input = new Scanner(System.in);
 
+	        System.out.println("Ingrese el sueldo");
+	        float sueldo = input.nextFloat();
+
+	        System.out.println("El IVA de su sueldo es de " + calcularIva(sueldo));
+	    }
+
+	    public static float calcularIva(float sueldo){
+	        float calculo = IVA * sueldo / 100;
+	        return calculo;
+	    }
 	}
-
-}
